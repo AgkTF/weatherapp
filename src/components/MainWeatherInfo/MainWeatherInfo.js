0646a6ab7c50886865860a28df32248df7cf0cc1
agkz1st <ahmed.agk22@gmail.com>
2019-03-25 22:12:02 +0200

    Add code for date and updating clock

--- src/components/MainWeatherInfo/MainWeatherInfo.js
+++ src/components/MainWeatherInfo/MainWeatherInfo.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../assets/weather-icons/css/weather-icons.min.css';
+import Moment from 'react-moment';
 import classes from './MainWeatherInfo.module.css';
 
-const mainWeatherInfo = () => (
-	<div className={classes.Container}>
-		<div className={classes.Header}>
-			<i className="wi wi-night-sleet-storm" />
-			33°<span>C</span>
-		</div>
-		<div className={classes.Subheader1}>Breezy starting this evening</div>
-		<div className={classes.Subheader2}>Aug. 7th | 11:13PM</div>
-	</div>
-);
+class MainWeatherInfo extends Component {
+	state = {
+		date: new Date(),
+		weatherIcon: 'night-sleet-storm',
+		weatherSummary: 'Breezy starting this evening',
+		temp: 33
+	};
 
-export default mainWeatherInfo;
+	tick = () => {
+		this.setState({ date: new Date() });
+	};
+
+	componentDidMount() {
+		this.timerId = setInterval(() => this.tick(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+
+	render() {
+		const date = this.state.date;
+		const hrs = date.getHours();
+		const mins = date.getMinutes();
+		const secs = date.getSeconds();
+
+		return (
+			<div className={classes.Container}>
+				<div className={classes.Header}>
+					<i className={`wi wi-${this.state.weatherIcon}`} />
+					{this.state.temp}°<span>C</span>
+				</div>
+				<div className={classes.Subheader1}>
+					{this.state.weatherSummary}
+				</div>
+				<div className={classes.Subheader2}>
+					<Moment format="dddd, MMMM Do">{this.state.date}</Moment> |{' '}
+					{hrs}:{mins}:{secs <= 9 ? `0${secs}` : secs}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default MainWeatherInfo;
