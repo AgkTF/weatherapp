6ed5008a0ac208e6f34af152d92d48d6026dd4fa
agkz1st <ahmed.agk22@gmail.com>
2019-03-30 10:22:00 +0200

    Add code for getCurrentLocation Button

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react';
 import MapboxAutocomplete from 'react-mapbox-autocomplete';
 import classes from './Controls.module.css';
 
-const controls = (props) => {
-	return (
-		<div className={classes.Controls}>
-			<div>
-				<Button icon>
-					<Icon name="location arrow" />
-				</Button>
-			</div>
-			<div style={{ width: '45%' }}>
-				<MapboxAutocomplete
-					publicKey={process.env.REACT_APP_MAPBOX_PUBLIC_KEY}
-					onSuggestionSelect={props.selected}
-					placeholder="Search Cities..."
-				/>
-			</div>
-		</div>
-	);
-};
+class Controls extends Component {
+	// componentDidUpdate(prevProps) {
+	// 	if(prevProps.loading === this.props.loading) {
+	// 		console.log('will not update');
+	// 	} else {
 
-export default controls;
+	// 	}
+	// }
+
+	render() {
+		return (
+			<div className={classes.Controls}>
+				<div>
+					<Button
+						icon
+						onClick={this.props.clicked}
+						loading={this.props.locating}
+					>
+						<Icon name={this.props.btnIcon} />
+					</Button>
+				</div>
+				<div style={{ width: '45%' }}>
+					<MapboxAutocomplete
+						publicKey={process.env.REACT_APP_MAPBOX_PUBLIC_KEY}
+						onSuggestionSelect={this.props.selected}
+						placeholder="Search Cities..."
+					/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Controls;
+
+// This component is turned into a stateful component
+// just to use componentdidupdate life cycle hook
+// this make a perfect use case for react Hooks
+// not now though.
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -12,13 +12,37 @@ class Layout extends Component {
 
 	_suggestionSelect = (result, lat, lng, text) => {
 		console.log(result, lat, lng, text);
+
+		// update the location object immutably
+		const updatedLocation = { ...this.state.location };
+		updatedLocation.city = result;
+		updatedLocation.lat = lat;
+		updatedLocation.lng = lng;
+
 		this.setState({
-			location: {
-				city: result,
-				lat: parseFloat(lat),
-				lng: parseFloat(lng)
-			}
+			location: updatedLocation
 		});
+	};
+
+	fetchCurrentLocationHandler = () => {
+		const success = (position) => {
+			const latitude = position.coords.latitude;
+			const longitude = position.coords.longitude;
+			console.log(`Latitude: ${latitude}\nLongitude: ${longitude}`);
+		};
+
+		const error = () => {
+			console.log('Unable to retrieve your location');
+			this.setState({ btnIcon: 'exclamation' });
+		};
+
+		if (!navigator.geolocation) {
+			console.log('Geolocation is not supported by your browser');
+			this.setState({ btnIcon: 'exclamation triangle' });
+		} else {
+			console.log('locating...');
+			navigator.geolocation.getCurrentPosition(success, error);
+		}
 	};
 
 	render() {
@@ -33,6 +57,9 @@ class Layout extends Component {
 					<Controls
 						location={this.state.location}
 						selected={this._suggestionSelect}
+						clicked={this.fetchCurrentLocationHandler}
+						locating={this.state.locating}
+						btnIcon={this.state.btnIcon}
 					/>
 				</header>
 				<main>
